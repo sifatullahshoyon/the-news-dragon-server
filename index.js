8380779d8e70063961fd14473278971511f6a421
freelancersifat627e <freelancersifat627@gmail.comm>
2023-12-27 01:31:35 +0600

    added news.json file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT ||5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/' , (req , res) => {
     res.send('Dragon is running');
@@ -14,7 +15,29 @@ app.get('/' , (req , res) => {
 
 app.get('/categories' , (req , res) => {
     res.send(categories);
-})
+});
+
+app.get('/categories/:id' , (req , res) => {
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news);
+    }
+    else{
+        const categoriesNews = news?.filter(category => parseInt(category.category_id) === id);
+        res.send(categoriesNews);
+    }
+});
+
+app.get('/news' , (req , res) => {
+    res.send(news);
+});
+
+
+app.get('/news/:id' , (req , res) => {
+    const id = req.params.id;
+    const selectedNews = news?.find(n => n._id === id);
+    res.send(selectedNews);
+});
 
 
 app.listen(port , () => {
