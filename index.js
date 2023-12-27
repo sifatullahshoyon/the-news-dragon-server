3a2afc2369e9fae39b52fab6d014b887add3d2d1
freelancersifat627e <freelancersifat627@gmail.comm>
2023-12-28 00:45:55 +0600

    delete some code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 // const port = process.ENV.PORT || 5000;
 const port = process.env.PORT ||5000;
 
-app.use(cors());
+// app.use(cors());
 
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
